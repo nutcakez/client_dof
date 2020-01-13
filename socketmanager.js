7d66a1614d0ebff886e3d34e2ae98aea7cadae5b
nutcakez <knorbert91@gmail.com>
2020-01-13 22:33:00 +0100

    1

--- socketmanager.js
+++
@@ -1,6 +0,0 @@
-var cardmanager=require('./sockets.js');
-console.log(cardmanager)
-const socket = cardmanager('localhost:2000');
-
-exports.socket=socket
-exports.alma="pew"
--- stuff.js
+++
@@ -1,6 +0,0 @@
-    setInterval(function(){
-        mynumber++;
-        console.log("pewpew")
-    },1000)
-let mynumber=0
-exports.number=mynumber
