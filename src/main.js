b17342e48b73d3c4a02019ff0a3179f300df0182
nutcakez <knorbert91@gmail.com>
2020-10-02 08:55:39 +0200

    Added vuex

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
-import { store } from './store'
+import { store } from '../src/store/store'
 
 Vue.config.productionTip = false
 Vue.use(BootstrapVue)
